9adf1906ef1b75555db1cd5596ac7967bcc29457
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-09 20:11:55 +0900

    fix square componenet props

--- src/square.tsx
+++ src/square.tsx
@@ -1,11 +1,30 @@
 import * as React from "react";
 
 interface SquareProps {
-  value: number;
+  value: string;
 }
 
-export class Square extends React.Component<SquareProps> {
+interface SquareState {
+  value: string;
+}
+
+export class Square extends React.Component<SquareProps, SquareState> {
+  static defaultProps: SquareProps = {
+    value: "",
+  };
+
+  constructor(props: SquareProps) {
+    super(props);
+    this.state = {
+      value: "",
+    };
+  }
+
   render() {
-    return <button className="square">{this.props.value}</button>;
+    return (
+      <button className="square" onClick={() => this.setState({ value: "X" })}>
+        {this.state.value}
+      </button>
+    );
   }
 }
