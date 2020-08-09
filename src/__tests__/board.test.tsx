08da0a36baa8ea80c40594f69f0f37a9c5e1cc97
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-09 18:29:35 +0900

    test

--- src/__tests__/board.test.tsx
+++ src/__tests__/board.test.tsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import {Board} from '../board'
-import renderer from 'react-test-renderer'
+import React from "react";
+import { Board } from "../board";
+import renderer from "react-test-renderer";
 
-test('Board class', ()=>{
-  const component = renderer.create(
-    <Board/>,
-  )
-  const tree = component.toJSON()
-  expect(tree).toMatchSnapshot()
-})
+test("Board class", () => {
+  const component = renderer.create(<Board />);
+  const tree = component.toJSON();
+  expect(tree).toMatchSnapshot();
+});
