19430546f0f7ab8cc473de0d0140a0a792e1fd0a
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-13 23:36:59 +0900

    Revert "wip"
    This reverts commit 9cd5bcfd835c6663ee182df5c0601e103644b173.

--- jest-puppeteer.config.js
+++
@@ -1,9 +0,0 @@
-const headless = false;
-module.exports = {
-  launch: {
-    dumpio: true,
-    headless: headless ? true : false,
-    executablePath: headless ? "/usr/bin/chromium" : "/usr/bin/google-chrome",
-  },
-  browserContext: "default",
-};
