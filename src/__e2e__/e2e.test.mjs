b8e5780cf24aa9e9d7c60ed7cb42440c8efffc96
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-14 20:06:20 +0900

    fix ci

--- src/__e2e__/e2e.test.mjs
+++ src/__e2e__/e2e.test.mjs
@@ -4,7 +4,7 @@ import puppeteer from 'puppeteer'
   const browser = await puppeteer.launch({ ignoreHTTPSErrors: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
   const page = await browser.newPage();
   await page.goto('http://localhost:3000', {
-          waitUntil: 'networkidle',
+          waitUntil: 'networkidle2',
           timeout: 3000000
         });
   await page.screenshot({path: 'example.png'});
