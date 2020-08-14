556b7b1ee63dd953cfb295d626761ba118c9470f
kazukinakamuraKN <kajirikajiri@gmail.com>
2020-08-14 20:45:44 +0900

    fix ci

--- src/__e2e__/e2e.test.mjs
+++ src/__e2e__/e2e.test.mjs
@@ -7,6 +7,7 @@ import puppeteer from 'puppeteer'
     'Accept-Language': 'en-US,en;q=0.9'
   });
   await page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36');
+  await page.waitFor(10000)
   await page.goto('http://localhost:3000')
   await page.screenshot({path: 'example.png'});
   await page.evaluate(() => {
